21dccf6e6c4ac0f3b2f54eb5bbe6a31ad04da2fb
AlikhanBaisholanov <alikh3380944@gmail.com>
2020-02-01 12:18:13 +0600

    Lab1

---
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar? = findViewById(R.id.toolbar)
+
+    }
+}
