f6b93e9a230fd0bd629ad9f44765504eaef53950
AlikhanBaisholanov <alikh3380944@gmail.com>
2020-04-05 17:48:58 +0600

    favourites fixing

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -17,7 +17,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar? = findViewById(R.id.toolbar)
-
     }
 }
